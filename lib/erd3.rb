a2558a3b63c0990fdcd8aa476ef7ec43ee913280
ken <block24block@gmail.com>
2017-09-20 23:17:33 +0800

    generate graph just like https://bl.ocks.org/mbostock/7607999

--- lib/erd3.rb
+++ lib/erd3.rb
@@ -1,6 +1,51 @@
 require "erd3/version"
 require "erd3/railtie" if defined? Rails
+require "rails_erd/domain"
+require "erb"
 
 module Erd3
-  # Your code goes here...
+  class Circle
+
+    NAME = "name".freeze
+    IMPORTS = "imports".freeze
+
+    class << self
+      def create
+        File.open(output_file, 'w') do |f|
+          f.puts ERB.new(File.read(template_file)).result(binding)
+        end
+      end
+
+      def data
+        domain = RailsERD::Domain.generate
+
+        dests = domain.relationships.group_by(&:destination).map do |dest, srcs|
+          {NAME => dest.model.to_s, IMPORTS => srcs.map{ |s| s.source.model.to_s }}
+        end
+
+        models_with_src = dests.map{ |h| h[NAME] }
+
+        models_without_src = domain.entities.reduce([]) do |rs, e|
+          model_name = e.model.to_s
+          rs << model_name if e.model != ApplicationRecord && !models_with_src.include?(model_name)
+          rs
+        end
+
+        no_srcs = models_without_src.map do |model|
+          {NAME => model, IMPORTS => []}
+        end
+
+        data = (dests + no_srcs).sort_by{ |h| h[NAME] }
+      end
+
+      def template_file
+        File.join __dir__, 'templates', 'circle.erb'
+      end
+
+      def output_file
+        File.join Rails.root.to_s, 'circle.html'
+      end
+    end
+
+  end
 end
--- lib/erd3/tasks.rake
+++ lib/erd3/tasks.rake
@@ -4,20 +4,7 @@ load "#{spec.gem_dir}/lib/rails_erd/tasks.rake"
 
 namespace :erd3 do
   task :generate => ['erd:check_dependencies', 'erd:options', 'erd:load_models'] do
-
-    require "rails_erd/domain"
-    domain = RailsERD::Domain.generate
-    domain.relationships.each do |relationship|
-      line = if relationship.indirect? then "-.-" else "-" end
-      arrow =
-        case
-        when relationship.one_to_one?   then "1#{line}1>"
-        when relationship.one_to_many?  then "1#{line}*>"
-        when relationship.many_to_many? then "*#{line}*>"
-        end
-      puts "[#{relationship.source}] #{arrow} [#{relationship.destination}]"
-    end
-
+    Erd3::Circle.create
   end
 end
 
