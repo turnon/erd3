eb35c909dbb5da0e5f1578382345b6e800aa85b8
ken <block24block@gmail.com>
2017-09-23 22:10:00 +0800

    extract common methods to super class

--- lib/erd3.rb
+++ lib/erd3.rb
@@ -4,48 +4,70 @@ require "rails_erd/domain"
 require "erb"
 
 module Erd3
-  class Circle
+
+  class Base
+
+    attr_reader :domain
+
+    def initialize
+      @domain = RailsERD::Domain.generate
+    end
+
+    def create
+      calculate
+      write
+    end
+
+    private
+
+    def write
+      File.open(output_path, 'w') do |f|
+        f.puts ERB.new(File.read(template_path)).result(binding)
+      end
+    end
+
+    def template_path
+      File.join __dir__, 'templates', "#{file_name}.erb"
+    end
+
+    def output_path
+      File.join Rails.root.to_s, "#{file_name}.html"
+    end
+
+    def file_name
+      @file_name ||= "#{self.class.to_s.sub(/.*::/, '').gsub(/(.)([A-Z])/, '\1_\2').downcase}"
+    end
+  end
+
+  class Circle < Base
 
     NAME = "name".freeze
     IMPORTS = "imports".freeze
 
-    class << self
-      def create
-        File.open(output_file, 'w') do |f|
-          f.puts ERB.new(File.read(template_file)).result(binding)
-        end
+    def calculate
+      domain = RailsERD::Domain.generate
+
+      dests = domain.relationships.group_by(&:destination).map do |dest, srcs|
+        {NAME => dest.model.to_s, IMPORTS => srcs.map{ |s| s.source.model.to_s }}
       end
 
-      def data
-        domain = RailsERD::Domain.generate
+      models_with_src = dests.map{ |h| h[NAME] }
 
-        dests = domain.relationships.group_by(&:destination).map do |dest, srcs|
-          {NAME => dest.model.to_s, IMPORTS => srcs.map{ |s| s.source.model.to_s }}
-        end
-
-        models_with_src = dests.map{ |h| h[NAME] }
-
-        models_without_src = domain.entities.reduce([]) do |rs, e|
-          model_name = e.model.to_s
-          rs << model_name if e.model != ApplicationRecord && !models_with_src.include?(model_name)
-          rs
-        end
-
-        no_srcs = models_without_src.map do |model|
-          {NAME => model, IMPORTS => []}
-        end
-
-        data = (dests + no_srcs).sort_by{ |h| h[NAME] }
+      models_without_src = domain.entities.reduce([]) do |rs, e|
+        model_name = e.model.to_s
+        rs << model_name if e.model != ApplicationRecord && !models_with_src.include?(model_name)
+        rs
       end
 
-      def template_file
-        File.join __dir__, 'templates', 'circle.erb'
+      no_srcs = models_without_src.map do |model|
+        {NAME => model, IMPORTS => []}
       end
 
-      def output_file
-        File.join Rails.root.to_s, 'circle.html'
-      end
+      @data = (dests + no_srcs).sort_by{ |h| h[NAME] }
     end
 
+  end
+
+  class Force < Base
   end
 end
